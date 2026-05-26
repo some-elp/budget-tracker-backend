9fb21e0ad9332c8adb3715f930ac59c09102dfbe
Jack Li <ineedelp66@gmail.com>
2026-05-26 13:47:47 -0400

    fully completed authentication, and added a little test route for tokens.

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,20 +3,24 @@ from sqlalchemy.orm import Session
 from app.db import get_db
 from app.models.user import User
 from app.core.auth import hash_password, verify_password, create_access_token
+from app.schemas.auth import RegisterRequest
+from app.schemas.auth import LoginRequest
+from app.core.deps import get_current_user
 
-router = APIRouter(prefix="/auth", tags=["auth"])
+
+router = APIRouter(tags=["auth"])
 
 # POST: Register user
 @router.post("/register")
-def register(email: str, password: str, db: Session = Depends(get_db)):
-  existing = db.query(User).filter(User.email == email).first()
+def register(data: RegisterRequest, db: Session = Depends(get_db)):
+  existing = db.query(User).filter(User.email == data.email).first()
 
   if existing:
     raise HTTPException(status_code=400, detail="Email already registered")
 
   user = User(
     email=email,
-    hashed_password=hash_password(password)
+    hashed_password=hash_password(data.password)
   )
 
   db.add(user)
@@ -27,13 +31,21 @@ def register(email: str, password: str, db: Session = Depends(get_db)):
 
 # POST: Login user
 @router.post("/login")
-def login(email: str, password: str, db: Session = Depends(get_db)):
-  user = db.query(User).filter(User.email == email).first()
+def login(data: LoginRequest, db: Session = Depends(get_db)):
+  user = db.query(User).filter(User.email == data.email).first()
 
-  if not user or not verify_password(password, user.hashed_password):
+  if not user or not verify_password(data.password, user.hashed_password):
     raise HTTPException(status_code=401, detail="Invalid credentials")
 
   token = create_access_token({"user_id": user.id})
 
   return {"access_token": token, "token_type": "bearer"}
+
+# GET: for testing protected routes.
+@router.get("/me")
+def get_me(current_user: User = Depends(get_current_user)):
+    return {
+        "id": current_user.id,
+        "email": current_user.email
+    }
 
---
+++ app/schemas/auth.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel, EmailStr, Field
+
+class RegisterRequest(BaseModel):
+  email: EmailStr
+  password: str
+
+class LoginRequest(BaseModel):
+  email: EmailStr
+  password: str
